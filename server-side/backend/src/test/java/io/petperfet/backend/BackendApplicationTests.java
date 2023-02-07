cbce55c09eb170133925323c62d47da6857aefad
Sarthak Mondal <raceison139@gmail.com>
2023-02-07 18:22:18 +0530

    07-02-2023 18:22

---
+++ server-side/backend/src/test/java/io/petperfet/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.petperfet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
